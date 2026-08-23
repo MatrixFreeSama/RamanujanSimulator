4fb6614f1dac1426c765622ded9e79dc8c8b1c7d
Kestis <des3505@outlook.com>
2026-08-24 04:55:30 +0800

    Add shared C17 implementation unit

---
+++ src/ramanujan_c_common.c
@@ -0,0 +1,10 @@
+#include "ramanujan_c_common.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ramanujan_c_common_core.inc"
+#include "ramanujan_c_common_qseries.inc"
+#include "ramanujan_c_common_tail.inc"
